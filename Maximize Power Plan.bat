b7f7c597f318f3ddc1207370549f6148c3e90228
Rhet0r1cal_ <meddakee2000@gmail.com>
2024-04-04 05:22:05 -0700

    Added MPP.bat

---
+++ Maximize Power Plan.bat
@@ -0,0 +1,19 @@
+:: WinScripts is property of Keegan (Rhet0r1cal) and can be used and modified for any use.
+:: Use this script at your own discresion.
+
+:: Init
+cls
+@echo off
+title WinScript
+
+:: Start
+echo WinScripts - Maximize Power Plan
+echo This script will align all of your power plan settings to optimize performance. This is only recommended for laptops that are usually plugged in often, like gaming laptops.
+
+:: Pause
+echo.
+echo Press ENTER to start the script. I suggest waiting until the script finishes before interacting.
+pause >nul
+
+:: Script
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
